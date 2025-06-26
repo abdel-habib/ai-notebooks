aa3616a4ecdcb2b47bc70e1ed708588d4f4012a6
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2025-06-27 01:01:04 +0300

    intermediate structural layers

--- utils/plots.py
+++ utils/plots.py
@@ -17,3 +17,27 @@ def linear_plot(x, y, title=''):
     plt.title(title)
     plt.grid(True)
     plt.show()
+
+def plot_histograms(histograms = {}):
+    """Plots histograms for the given data.
+    
+    Parameters:
+    histograms (list): A list of dictionaries where keys are histogram names and values are lists of data to plot.
+
+    Returns:
+    None
+    """
+    num_plots = len(histograms)
+    fig, axes = plt.subplots(1, num_plots, figsize=(5 * num_plots, 4))
+
+    if num_plots == 1:
+        axes = [axes]
+
+    for ax, data in zip(axes, histograms):
+        if not hasattr(data, 'values'):
+            raise ValueError("Each histogram data must have a 'value' attribute.")
+
+        ax.hist(data['values'], bins=30)
+        ax.set_title(data['name'])
+
+    plt.show()
