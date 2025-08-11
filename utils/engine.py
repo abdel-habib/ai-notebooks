739812d9400a71a55e70d459d74a00889ebe69b3
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2025-08-12 02:48:20 +0300

    modulating a torch experiment engine for reference

--- utils/engine.py
+++ utils/engine.py
@@ -2,6 +2,11 @@
 Contains functions for training and testing a PyTorch model.
 """
 import torch
+from torch.utils.tensorboard import SummaryWriter
+
+
+import time
+from pathlib import Path
 
 from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
@@ -16,12 +21,70 @@ def set_seeds(seed: int=42):
     torch.manual_seed(seed)
     # Set the seed for CUDA torch operations (ones that happen on the GPU)
     torch.cuda.manual_seed(seed)
+
+def set_TF32():
+    """Sets the TensorFloat-32 (TF32) mode for mixed precision training on supported GPUs.
+    """
+    if torch.cuda.is_available():
+        # Check the GPU score
+        GPU_SCORE = torch.cuda.get_device_capability()
+
+        if GPU_SCORE >= (8, 0):
+            torch.backends.cuda.matmul.allow_tf32 = True
+            torch.backends.cudnn.allow_tf32 = True
+            print("[INFO] TF32 mode enabled.")
+        else:
+            print("[INFO] TF32 mode not supported on this device.")
+    else:
+        print("[INFO] No CUDA device available, TF32 mode not set.")
+
+def create_writer(experiment_name: str, 
+                  model_name: str, 
+                  extra: str = None) -> SummaryWriter:
+    """Creates a torch.utils.tensorboard.writer.SummaryWriter() instance saving to a specific log_dir.
+
+    log_dir is a combination of runs/timestamp/experiment_name/model_name/extra.
+
+    Where timestamp is the current date in YYYY-MM-DD format.
+
+    Args:
+        experiment_name (str): Name of experiment.
+        model_name (str): Name of model.
+        extra (str, optional): Anything extra to add to the directory. Defaults to None.
+
+    Returns:
+        SummaryWriter: Instance of a writer saving to log_dir.
+
+    Example usage:
+        # Create a writer saving to "runs/2022-06-04/data_10_percent/effnetb2/5_epochs/"
+        writer = create_writer(experiment_name="data_10_percent",
+                               model_name="effnetb2",
+                               extra="5_epochs")
+        # The above is the same as:
+        writer = SummaryWriter(log_dir="runs/2022-06-04/data_10_percent/effnetb2/5_epochs/")
+    """
+    from datetime import datetime
+    import os
+
+    # Get timestamp of current date (all experiments on certain day live in same folder)
+    timestamp = datetime.now().strftime("%Y-%m-%d") # returns current date in YYYY-MM-DD format
+
+    if extra:
+        # Create log directory path
+        log_dir = os.path.join("runs", timestamp, experiment_name, model_name, extra)
+    else:
+        log_dir = os.path.join("runs", timestamp, experiment_name, model_name)
+        
+    print(f"[INFO] Created SummaryWriter, saving to: {log_dir}...")
+    return SummaryWriter(log_dir=log_dir)
     
-def train_step(model: torch.nn.Module, 
+def train_step(epoch: int,
+               model: torch.nn.Module, 
                dataloader: torch.utils.data.DataLoader, 
                loss_fn: torch.nn.Module, 
                optimizer: torch.optim.Optimizer,
-               device: torch.device) -> Tuple[float, float]:
+               device: torch.device,
+               disable_progress_bar: bool = False) -> Tuple[float, float]:
     """Trains a PyTorch model for a single epoch.
 
     Turns a target PyTorch model to training mode and then
@@ -29,17 +92,17 @@ def train_step(model: torch.nn.Module,
     pass, loss calculation, optimizer step).
 
     Args:
-    model: A PyTorch model to be trained.
-    dataloader: A DataLoader instance for the model to be trained on.
-    loss_fn: A PyTorch loss function to minimize.
-    optimizer: A PyTorch optimizer to help minimize the loss function.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+        model: A PyTorch model to be trained.
+        dataloader: A DataLoader instance for the model to be trained on.
+        loss_fn: A PyTorch loss function to minimize.
+        optimizer: A PyTorch optimizer to help minimize the loss function.
+        device: A target device to compute on (e.g. "cuda" or "cpu").
 
     Returns:
-    A tuple of training loss and training accuracy metrics.
-    In the form (train_loss, train_accuracy). For example:
+        A tuple of training loss and training accuracy metrics.
+        In the form (train_loss, train_accuracy). For example:
 
-    (0.1112, 0.8743)
+        (0.1112, 0.8743)
     """
     # Put model in train mode
     model.train()
@@ -48,7 +111,14 @@ def train_step(model: torch.nn.Module,
     train_loss, train_acc = 0, 0
 
     # Loop through data loader data batches
-    for batch, (X, y) in enumerate(dataloader):
+    progress_bar = tqdm(
+            enumerate(dataloader), 
+            desc=f"Training Epoch {epoch}", 
+            total=len(dataloader),
+            disable=disable_progress_bar
+        )
+
+    for batch, (X, y) in progress_bar:
         # Send data to target device
         X, y = X.to(device), y.to(device)
 
@@ -68,35 +138,46 @@ def train_step(model: torch.nn.Module,
         # 5. Optimizer step
         optimizer.step()
 
-        # Calculate and accumulate accuracy metric across all batches
+        # Calculate and accumulate accuracy metrics across all batches
         y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
         train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+        # Update progress bar
+        progress_bar.set_postfix(
+                {
+                    "train_loss": train_loss / (batch + 1),
+                    "train_acc": train_acc / (batch + 1),
+                }
+            )
+
 
     # Adjust metrics to get average loss and accuracy per batch 
     train_loss = train_loss / len(dataloader)
     train_acc = train_acc / len(dataloader)
     return train_loss, train_acc
 
-def test_step(model: torch.nn.Module, 
+def test_step(epoch: int,
+              model: torch.nn.Module, 
               dataloader: torch.utils.data.DataLoader, 
               loss_fn: torch.nn.Module,
-              device: torch.device) -> Tuple[float, float]:
+              device: torch.device,
+              disable_progress_bar: bool = False) -> Tuple[float, float]:
     """Tests a PyTorch model for a single epoch.
-
+     
     Turns a target PyTorch model to "eval" mode and then performs
     a forward pass on a testing dataset.
 
     Args:
-    model: A PyTorch model to be tested.
-    dataloader: A DataLoader instance for the model to be tested on.
-    loss_fn: A PyTorch loss function to calculate loss on the test data.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+        model: A PyTorch model to be tested.
+        dataloader: A DataLoader instance for the model to be tested on.
+        loss_fn: A PyTorch loss function to calculate loss on the test data.
+        device: A target device to compute on (e.g. "cuda" or "cpu").
 
     Returns:
-    A tuple of testing loss and testing accuracy metrics.
-    In the form (test_loss, test_accuracy). For example:
+        A tuple of testing loss and testing accuracy metrics.
+        In the form (test_loss, test_accuracy). For example:
 
-    (0.0223, 0.8985)
+        (0.0223, 0.8985)
     """
     # Put model in eval mode
     model.eval() 
@@ -104,10 +185,18 @@ def test_step(model: torch.nn.Module,
     # Setup test loss and test accuracy values
     test_loss, test_acc = 0, 0
 
+    # Loop through data loader data batches
+    progress_bar = tqdm(
+        enumerate(dataloader), 
+        desc=f"Testing Epoch {epoch}", 
+        total=len(dataloader),
+        disable=disable_progress_bar
+    )
+
     # Turn on inference context manager
-    with torch.inference_mode():
+    with torch.no_grad(): # no_grad() required for PyTorch 2.0, I found some errors with `torch.inference_mode()`, please let me know if this is not the case
         # Loop through DataLoader batches
-        for batch, (X, y) in enumerate(dataloader):
+        for batch, (X, y) in progress_bar:
             # Send data to target device
             X, y = X.to(device), y.to(device)
 
@@ -122,6 +211,14 @@ def test_step(model: torch.nn.Module,
             test_pred_labels = test_pred_logits.argmax(dim=1)
             test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
 
+            # Update progress bar
+            progress_bar.set_postfix(
+                {
+                    "test_loss": test_loss / (batch + 1),
+                    "test_acc": test_acc / (batch + 1),
+                }
+            )
+
     # Adjust metrics to get average loss and accuracy per batch 
     test_loss = test_loss / len(dataloader)
     test_acc = test_acc / len(dataloader)
@@ -133,7 +230,9 @@ def train(model: torch.nn.Module,
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int,
-          device: torch.device) -> Dict[str, List]:
+          device: torch.device,
+          writer: SummaryWriter,
+          disable_progress_bar: bool = False) -> Dict[str, List]:
     """Trains and tests a PyTorch model.
 
     Passes a target PyTorch models through train_step() and test_step()
@@ -143,57 +242,73 @@ def train(model: torch.nn.Module,
     Calculates, prints and stores evaluation metrics throughout.
 
     Args:
-    model: A PyTorch model to be trained and tested.
-    train_dataloader: A DataLoader instance for the model to be trained on.
-    test_dataloader: A DataLoader instance for the model to be tested on.
-    optimizer: A PyTorch optimizer to help minimize the loss function.
-    loss_fn: A PyTorch loss function to calculate loss on both datasets.
-    epochs: An integer indicating how many epochs to train for.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+        model: A PyTorch model to be trained and tested.
+        train_dataloader: A DataLoader instance for the model to be trained on.
+        test_dataloader: A DataLoader instance for the model to be tested on.
+        optimizer: A PyTorch optimizer to help minimize the loss function.
+        loss_fn: A PyTorch loss function to calculate loss on both datasets.
+        epochs: An integer indicating how many epochs to train for.
+        device: A target device to compute on (e.g. "cuda" or "cpu").
+        writer: A TensorBoard SummaryWriter instance for logging.
+        disable_progress_bar: A boolean indicating whether to disable the progress bar.
 
     Returns:
-    A dictionary of training and testing loss as well as training and
-    testing accuracy metrics. Each metric has a value in a list for 
-    each epoch.
-    In the form: {train_loss: [...],
-              train_acc: [...],
-              test_loss: [...],
-              test_acc: [...]} 
-    For example if training for epochs=2: 
-             {train_loss: [2.0616, 1.0537],
-              train_acc: [0.3945, 0.3945],
-              test_loss: [1.2641, 1.5706],
-              test_acc: [0.3400, 0.2973]} 
+        A dictionary of training and testing loss as well as training and
+        testing accuracy metrics. Each metric has a value in a list for 
+        each epoch.
+        In the form: {train_loss: [...],
+                    train_acc: [...],
+                    test_loss: [...],
+                    test_acc: [...]} 
+        For example if training for epochs=2: 
+                    {train_loss: [2.0616, 1.0537],
+                    train_acc: [0.3945, 0.3945],
+                    test_loss: [1.2641, 1.5706],
+                    test_acc: [0.3400, 0.2973]} 
     """
     # Create empty results dictionary
     results = {"train_loss": [],
-               "train_acc": [],
-               "test_loss": [],
-               "test_acc": []
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": [],
+        "train_epoch_time": [],
+        "test_epoch_time": []
     }
-    
-    # Make sure model on target device
-    model.to(device)
 
     # Loop through training and testing steps for a number of epochs
-    for epoch in tqdm(range(epochs)):
-        train_loss, train_acc = train_step(model=model,
-                                          dataloader=train_dataloader,
-                                          loss_fn=loss_fn,
-                                          optimizer=optimizer,
-                                          device=device)
-        test_loss, test_acc = test_step(model=model,
-          dataloader=test_dataloader,
-          loss_fn=loss_fn,
-          device=device)
+    for epoch in tqdm(range(epochs), disable=disable_progress_bar):
+        # Perform training step and time it
+        train_epoch_start_time = time.time()
+        train_loss, train_acc = train_step(epoch=epoch, 
+                                            model=model,
+                                            dataloader=train_dataloader,
+                                            loss_fn=loss_fn,
+                                            optimizer=optimizer,
+                                            device=device,
+                                            disable_progress_bar=disable_progress_bar)
+        train_epoch_end_time = time.time()
+        train_epoch_time = train_epoch_end_time - train_epoch_start_time
+        
+        # Perform testing step and time it
+        test_epoch_start_time = time.time()
+        test_loss, test_acc = test_step(epoch=epoch,
+                                        model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn,
+                                        device=device,
+                                        disable_progress_bar=disable_progress_bar)
+        test_epoch_end_time = time.time()
+        test_epoch_time = test_epoch_end_time - test_epoch_start_time
 
         # Print out what's happening
         print(
-          f"Epoch: {epoch+1} | "
-          f"train_loss: {train_loss:.4f} | "
-          f"train_acc: {train_acc:.4f} | "
-          f"test_loss: {test_loss:.4f} | "
-          f"test_acc: {test_acc:.4f}"
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f} | "
+            f"train_epoch_time: {train_epoch_time:.4f} | "
+            f"test_epoch_time: {test_epoch_time:.4f}"
         )
 
         # Update results dictionary
@@ -201,6 +316,60 @@ def train(model: torch.nn.Module,
         results["train_acc"].append(train_acc)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
+        results["train_epoch_time"].append(train_epoch_time)
+        results["test_epoch_time"].append(test_epoch_time)
+
+        # Log metrics to TensorBoard
+        if writer:
+            # Add results to SummaryWriter
+            writer.add_scalars(main_tag="Loss", 
+                                tag_scalar_dict={
+                                    "train_loss": train_loss,
+                                    "test_loss": test_loss
+                                },
+                                global_step=epoch)
+            writer.add_scalars(main_tag="Accuracy", 
+                                tag_scalar_dict={
+                                    "train_acc": train_acc,
+                                    "test_acc": test_acc
+                                },
+                                global_step=epoch)
+            writer.add_scalars(main_tag="Epoch Time",
+                                tag_scalar_dict={
+                                    "train_epoch_time": train_epoch_time,
+                                    "test_epoch_time": test_epoch_time
+                                },
+                                global_step=epoch)
 
     # Return the filled results at the end of the epochs
     return results
+
+def save_model(model: torch.nn.Module,
+               target_dir: str,
+               model_name: str):
+  """Saves a PyTorch model to a target directory.
+
+  Args:
+    model: A target PyTorch model to save.
+    target_dir: A directory for saving the model to.
+    model_name: A filename for the saved model. Should include
+      either ".pth" or ".pt" as the file extension.
+
+  Example usage:
+    save_model(model=model_0,
+               target_dir="models",
+               model_name="05_going_modular_tingvgg_model.pth")
+  """
+  # Create target directory
+  target_dir_path = Path(target_dir)
+  target_dir_path.mkdir(parents=True,
+                        exist_ok=True)
+
+  # Create model save path
+  assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pt' or '.pth'"
+  model_save_path = target_dir_path / model_name
+
+  # Save the model state_dict()
+  print(f"[INFO] Saving model to: {model_save_path}")
+  torch.save(obj=model.state_dict(),
+             f=model_save_path)
